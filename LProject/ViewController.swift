d543265dbb093a705d3d5ef949eca01b4bf0b16c
C2QJG01SDRJD <C2QJG01SDRJD@c2qjg01drjdsmbp.hsd1.ga.comcast.net>
2019-02-26 17:32:02 -0500

    Initial Commit

---
+++ LProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LProject
+//
+//  Created by C2QJG01SDRJD on 2/26/19.
+//  Copyright © 2019 C2QJG01SDRJD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
