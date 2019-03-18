7cd9a09c737b0a015e602f145c515840ff2a2a33
C2QJG01SDRJD <C2QJG01SDRJD@ACA814FB.ipt.aol.com>
2019-03-18 12:23:38 -0400

    first commit

---
+++ LProject/WeatherProt.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherProt.swift
+//  LProject
+//
+//  Created by C2QJG01SDRJD on 2/26/19.
+//  Copyright © 2019 C2QJG01SDRJD. All rights reserved.
+//
+
+import Foundation
---
+++ WeatherTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherTableViewCell.swift
+//  LProject
+//
+//  Created by C2QJG01SDRJD on 3/8/19.
+//  Copyright © 2019 C2QJG01SDRJD. All rights reserved.
+//
+
+import Foundation
